90e002205407faf72be3ee2d97451d4c378e30e3
Teeandnarita <128231239+Teeandnarita@users.noreply.github.com>
2025-10-05 10:26:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
-    function sayHello(){ // Finished
-      const changeHello = document.getElementById("main-titlee")
-      const changeLoading = document.getElementById("status-one")
-      changeHello.innerText = "สวัสดี Tee!"
-      changeLoading.innerText = "Finished Loading"
-    }
-    
-    function hiName(){// Finished
-      const name = document.getElementById("nameInput-two").value;
-      const addHello = document.getElementById("output-two")
-      addHello.innerText = "สวัสดี "+ name
-    }
-    
-    function changeStyle(){ // Finish
-      const styleChange = document.getElementById("title-three").style.color = "blue";
-      const twoStyleChange = document.getElementById("title-three").style.fontSize = "40px";
-    }
+function sayHello() {
+  const changeHello = document.getElementById("main-titlee");
+  const changeLoading = document.getElementById("status-one");
+  changeHello.innerText = "สวัสดี Tee!";
+  changeLoading.innerText = "Finished Loading";
+}
 
-function addStuff(){
-      const stuffInput = document.getElementById("inputBox-four").value;
-      const add = document.getElementById("display-four")
-      add.innerText = document.getElementById("display-four").innerText + document.getElementById("inputBox-four").value
-    }
+sayHello();
+
+function hiName() {
+  const name = document.getElementById("nameInput-two").value;
+  const addHello = document.getElementById("output-two");
+  addHello.innerText = "สวัสดี " + name;
+}
+
+hiName();
+
+function changeStyle() {
+  const styleChange = (document.getElementById("title-three").style.color =
+    "blue");
+  const twoStyleChange = (document.getElementById(
+    "title-three",
+  ).style.fontSize = "40px");
+}
+
+function addStuff() {
+  const stuffInput = document.getElementById("inputBox-four").value;
+  const add = document.getElementById("display-four");
+  add.innerText =
+    document.getElementById("display-four").innerText +
+    document.getElementById("inputBox-four").value;
+}
+
+var round = 0;
+function toggleSecret() {
+  const chanText = document.getElementById("secret");
+  if (round / 2 == 0) {
+    chanText.innerText = "";
+  } else {
+    chanText.innerText = "นี่คือข้อความลับ";
+  }
+  round += 1;
+}
